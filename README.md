df64a4f9b36bd42c0de0f324c3853cd189bb0e7f
deedee <k4georges@gmail.com>
2015-01-28 15:48:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+SW Badge Maker
+==============
+
+## Quick Install
+  The quickest way to get started with BadgeMaker is to clone the project and utilize it like this:
+
+  Install dependencies:
+
+    $ npm install
+
+  Add some files near main.js
+
+  Start the node application:
+
+    $ node main.js
+
+  Tada, now you have some pdf badges files
---
+++ main.js
@@ -0,0 +1,145 @@
+var PdfDoc = require('pdfkit'),
+    parse  = require('csv-parse'),
+    fs     = require('fs'),
+    _      = require('underscore'),
+    _s     = require('underscore.string');
+
+var outDir = './BADGES';
+
+fs.readdir('./', function (err, files)
+{
+    if (err)
+    {
+        throw err;
+    }
+
+    _.each(files, function (filename)
+    {
+        if (!_s.endsWith(filename, '.csv'))
+        {
+            return;
+        }
+        var outFile = outDir + '/' + filename.substr(0, filename.length - 4) + '.pdf';
+        console.info('Processing ' + filename + ' -> ' + outFile);
+
+        fs.readFile(filename, function (err, str)
+        {
+            if (err)
+            {
+                throw err;
+            }
+
+            parse(str, {columns: true}, function (err, data)
+            {
+                //console.log(data);
+
+                var doc = new PdfDoc({
+                        size: 'A4', /*595.28, 841.89*/
+                        margin: 0
+                    }),
+                    cardHeight = doc.page.height / 4,
+                    margin = 16;
+                doc.font('fonts/Roboto-Regular.ttf');
+
+                _.each(data, function (person, index)
+                {
+                    var byPage = 4,
+                        i = index % byPage;
+
+                    if (i == 0)
+                    {
+                        if (index != 0)
+                        {
+                            doc.addPage();
+                        }
+
+                        doc.rect(doc.page.width / 2, 0, 0, doc.page.height).stroke();
+
+                        for (var x = 0; x < byPage; x++)
+                        {
+                            doc.rect(0, x * cardHeight, doc.page.width, 0).stroke();
+                        }
+                    }
+
+                    if (i < 2)
+                    {
+                        doc.rotate(-90)
+                            .image('img/cantine.png', -cardHeight * i - 80 - margin, 210, {width: 80})
+                            .image('img/men.png', -cardHeight * (i + 1), doc.page.width / 2 - 75, {height: 75})
+                            .rotate(90);
+                    }
+                    else
+                    {
+                        doc.rotate(-90)
+                            .image('img/cantine.png', -cardHeight * (i + 1) + margin, 210, {width: 80})
+                            .image('img/menR.png', -cardHeight * i - 107, doc.page.width / 2 - 75, {height: 75})
+                            .rotate(90);
+                    }
+
+
+                    var capitalizeEach = function (str)
+                    {
+                        var tmp = str.toLowerCase();
+                        tmp = tmp.split(' ');
+                        var ret = '';
+                        _.each(tmp, function (part)
+                        {
+                            ret += _s.capitalize(part) + ' ';
+                        });
+                        return ret.slice(0, ret.length - 1);
+                    };
+
+                    doc.rotate(-90)
+                        .image('img/sw.png', -cardHeight * (i + 1) + margin, margin, {width: cardHeight - 2 * margin})
+                        .fontSize(26)
+                        .text(capitalizeEach(person['Prénom']), -cardHeight * (i + 1) + margin, 110, {
+                            width: cardHeight - 2 * margin,
+                            align: 'center',
+                            continued: true
+                        })
+                        .fontSize(20)
+                        .text('\n\n' + person['Nom'].toUpperCase(), {
+                            align: 'center',
+                            style: 'bold'
+                        })
+                        .rotate(90);
+
+                    doc.rotate(90)
+                        .image('img/sw_logo.jpg', cardHeight * i + 16, -570, {width: cardHeight - 2 * margin})
+                        .fontSize(14)
+                        .text('En cas de problème joindre', cardHeight * i, -doc.page.width + margin + 10, {
+                            width: cardHeight,
+                            align: 'center'
+                        })
+                        .image('img/building.png', cardHeight * i + 16, -520, {width: 16})
+                        .text('Jessica : 06 18 76 28 15', cardHeight * i + 40, -520, {
+                            width: cardHeight,
+                            align: 'left'
+                        })
+                        .image('img/robot.png', cardHeight * i + 16, -490, {width: 16})
+                        .text('Horacio : 06 10 50 18 34', cardHeight * i + 40, -490, {
+                            width: cardHeight,
+                            align: 'left'
+                        })
+                        .image('img/safety.png', cardHeight * i + 16, -460, {width: 16})
+                        .text('Noëlla : 06 12 12 66 40', cardHeight * i + 40, -460, {
+                            width: cardHeight,
+                            align: 'left'
+                        })
+                        .rotate(-90);
+                });
+
+                doc.output(function (pdfData)
+                {
+                    if (!fs.existsSync(outDir)){
+                        fs.mkdirSync(outDir);
+                    }
+
+                    var wstream = fs.createWriteStream(outFile);
+                    wstream.write(pdfData);
+                    wstream.end();
+                });
+            });
+        });
+    });
+});
